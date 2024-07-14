ef158a8fb61302f0893db04ff9295f9ee80e98ed
unknown <zyad.sallem.007@gmail.com>
2024-07-14 14:09:53 +0300

    z

--- src/schemas/ObstacleCreate.ts
+++ src/schemas/ObstacleCreate.ts
@@ -3,7 +3,8 @@ import { z } from 'zod'
 export const ObstacleCreateSchema = z.object({
     lat: z.number({ required_error: 'Lat required' }),
     lng: z.number({ required_error: 'Lng required' }),
-    type: z.enum(['A'], { required_error: 'type required' }),
+    // type: z.enum(['D50'], { required_error: 'type required' }),
+    type: z.string({required_error: 'Type is required'}),
     regionId: z.number({required_error: 'Region Id'}),
     userId: z.number({required_error: 'UserId is required'}),
     imageUrl: z.string({required_error: 'URL is required'})
